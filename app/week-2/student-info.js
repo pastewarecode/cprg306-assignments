ae6c5808a7374f055ce3c93053216a02219d31b0
Cody Tran <147000920+pastewarecode@users.noreply.github.com>
2024-09-11 16:42:30 -0600

    Styled repository link
    Made some minor changes to how my repository link is displayed

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -6,12 +6,12 @@ const StudentInfo = () => {
         <div>
             <h1>Cody Tran</h1>
 
-            <p>My Github Repository Link:</p>
+            <p>My Github Repository:
 
-            <Link legacyBehavior href="https://github.com/pastewarecode/cprg306-assignments" passHref>
-                <a target="_blank" style={{color:'blue', textDecoration:'underline'}}> Cody's Github Repository </a>
-            </Link>
-                
+                <Link legacyBehavior href="https://github.com/pastewarecode/cprg306-assignments" passHref>
+                    <a target="_blank" style={{color:'blue', textDecoration:'underline'}}>Repository Link</a>
+                </Link>
+            </p>
         </div>
     );
 };
