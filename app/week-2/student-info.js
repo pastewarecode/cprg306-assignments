3959774700ac38cf12c7f7329aa8092d14198759
Cody Tran <147000920+pastewarecode@users.noreply.github.com>
2024-09-11 14:42:43 -0600

    page.js created

---
+++ app/week-2/student-info.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const StudentInfo = () => {
+    return (
+        <div>
+            <h1>Cody Tran</h1>
+            <p>
+                My Github Repository:
+                <Link href="https://github.com/pastewarecode/cprg306-assignments"></Link>
+            </p>
+        </div>
+    );
+};
+
+export default StudentInfo;
