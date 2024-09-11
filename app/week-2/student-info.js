6f9d7fb85823382eba2c31ed67150c63fca27434
Cody Tran <147000920+pastewarecode@users.noreply.github.com>
2024-09-11 16:39:57 -0600

    React Component and hyperlink
    Displayed StudentInfo component below the heading tag
    
    Created working hyperlink to github repo within the component

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import Link from 'next/link'
 
 const StudentInfo = () => {
     return (
         <div>
             <h1>Cody Tran</h1>
-            <p>
-                My Github Repository:
-                <Link href="https://github.com/pastewarecode/cprg306-assignments"></Link>
-            </p>
+
+            <p>My Github Repository Link:</p>
+
+            <Link legacyBehavior href="https://github.com/pastewarecode/cprg306-assignments" passHref>
+                <a target="_blank" style={{color:'blue', textDecoration:'underline'}}> Cody's Github Repository </a>
+            </Link>
+                
         </div>
     );
 };
