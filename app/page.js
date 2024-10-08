b69bc1a7a13df4ac26504f85e6ec4eb9d6c95ef0
Cody Tran <147000920+pastewarecode@users.noreply.github.com>
2024-10-08 16:36:12 -0600

    more styling done

--- app/page.js
+++ app/page.js
@@ -8,23 +8,23 @@ export default function Page() {
         <h1 className="font-bold text-2xl p-4">CPRG 306: Web Development 2 - Assignments</h1>
 
         <div className="p-2 px-8 border-4 max-w-[300px] rounded-xl text-lg">
-        <Link legacyBehavior href='http://localhost:3000/week-2' passHref>
-            <a target="_blank" style={{color:'blue', textDecoration:'underline'}}>Week-2 Assignment</a>
-        </Link>
+          <Link legacyBehavior href='http://localhost:3000/week-2' passHref>
+              <a className='text-blue-700 hover:text-violet-900 underline' target="_blank">Week-2 Assignment</a>
+          </Link>
 
-        <br></br> 
-        <br></br> 
-        
-        <Link legacyBehavior href='http://localhost:3000/week-3' passHref>
-            <a target="_blank" style={{color:'blue', textDecoration:'underline'}}>Week-3 Assignment</a>
-        </Link>
+          <br></br> 
+          <br></br> 
+          
+          <Link legacyBehavior href='http://localhost:3000/week-3' passHref>
+              <a className='text-blue-700 hover:text-violet-900 underline' target="_blank">Week-3 Assignment</a>
+          </Link>
 
-        <br></br>
-        <br></br> 
+          <br></br>
+          <br></br> 
 
-        <Link legacyBehavior href='http://localhost:3000/week-4' passHref>
-            <a target="_blank" style={{color:'blue', textDecoration:'underline'}}>Week-4 Assignment</a>
-        </Link>
+          <Link legacyBehavior href='http://localhost:3000/week-4' passHref>
+              <a className='text-blue-700 hover:text-violet-900 underline' target="_blank">Week-4 Assignment</a>
+          </Link>
         </div>
 
 
