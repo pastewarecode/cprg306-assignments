dd33605ddada41c89ee9a3e2a6c7773e4a05f910
Cody Tran <147000920+pastewarecode@users.noreply.github.com>
2024-10-22 23:30:14 -0600

    finished week 6 w/o optional challenge
    Finished item-list sort functions
    finished buttons for sort options
    Done some basic styling for buttons

--- app/week-6/page.js
+++ app/week-6/page.js
@@ -1,6 +1,10 @@
+import ItemList from './item-list';
+
 export default function Page() {
-    <main>
-        <h1>Shopping List</h1>
-        <ItemList></ItemList>
-    </main>
+    return (
+        <main className='bg-blue-400'>
+            <h1 className='text-3xl font-bold p-2'>Shopping List</h1>
+            <ItemList></ItemList>
+        </main>
+    )
 }
