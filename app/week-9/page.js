e5ad9322b0ab3d3fd7c4836da5113738257bd1db
Cody Tran <147000920+pastewarecode@users.noreply.github.com>
2024-10-31 13:37:14 -0600

    week-9 step 4 complete
    must complete step 5-6 for landing page and protected page. INCOMPLETE COMPONENTS

--- app/week-9/page.js
+++ app/week-9/page.js
@@ -1,16 +1,28 @@
+"use client"
+
 // Import the useUserAuth hook
 import { useUserAuth } from "./_utils/auth-context";
+
+export default function() {
+    // Use the useUserAuth hook to get the user object and the login and logout functions
+    const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+    
+
+    // Sign in to Firebase with GitHub authentication (AWAIT STATEMENT MUST BE USED IN ASYNC FUNCTION)
+    await gitHubSignIn();
+    
+
+    // Sign out of Firebase
+    await firebaseSignOut();
  
-// Use the useUserAuth hook to get the user object and the login and logout functions
-const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
- 
-// Sign in to Firebase with GitHub authentication
-await gitHubSignIn();
- 
-// Sign out of Firebase
-await firebaseSignOut();
- 
+
+
 // Display some of the user's information
-<p>
-  Welcome, {user.displayName} ({user.email})
-</p>;
+    return (
+     <main>
+          <p>
+            Welcome, {user.displayName} ({user.email})
+          </p>;
+      </main>
+    )
+}
