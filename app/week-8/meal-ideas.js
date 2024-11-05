c10d0a9ed23e0665e03f4a0c6af4b9105573522b
Cody Tran <147000920+pastewarecode@users.noreply.github.com>
2024-11-05 07:53:01 -0700

    almost finished meal-ideas.js
    Still working on some bugs and testing but i have most of it done

--- app/week-8/meal-ideas.js
+++ app/week-8/meal-ideas.js
@@ -1,13 +1,46 @@
 "use client"
 import { useState, useEffect } from 'react';
 
-export default function MealIdeas(ingredients) {
+export default function MealIdeas(ingredient) {
 
-    const [meals, setMeals] = useState([])
+    const [meals, setMeals] = useState([]);
 
-    const fetchMealIdeas(ingredients)
+    async function fetchMealIdeas(ingredient) {
+        const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`);
+
+        const data = response.json();
+
+        return data.meals || [];
+    }
+
+    async function loadMealIdeas() {
+        const fetchedMeals = await fetchMealIdeas(ingredient);
+        setMeals(fetchedMeals);
+        }
+
+    useEffect(() => {
+        if(ingredient)
+        {
+            loadMealIdeas();
+        }
+    }, [ingredient]);
+
+    
 
     return (
+        <div>
+            <h1>Meal Ideas</h1>
 
-    )
+            <ul>
+                {meals.map(meal) => (
+                    <h2>Here are meal ideas with {ingredient}</h2>
+                
+                    <li key={meal.idMeal}>
+                        <h3>{meals.strMeal}</h3>
+                        <img src={meal.strMealThumb} alt={meal.strMeal} width="100"/>
+                    </li>
+                )}
+            </ul> 
+        </div>
+    );
 }
