c11135562943a454ec42365335e9a97590769e6a
Cody Tran <147000920+pastewarecode@users.noreply.github.com>
2024-11-12 23:42:34 -0700

    Completed protected page and Sign In page
    Created the handle sign in and handle sign out function. Also returned it to the page.
    
    Then we created the protected page that displays the users info if user is not null.
    If user is null and they try to access the page directly, they will be prompted with a message telling them to log in.

--- app/week-9/page.js
+++ app/week-9/page.js
@@ -35,20 +35,44 @@ export default function SignInPage() {
 
 // Display some of the user's information
     return (
-     <main>
-          <header>
-            <h1>Firebase authentication</h1>
-          </header>
+        <main className="mb-5">
+            <header>
+                <h1 className="text-3xl text-center">FireBase Authentication</h1>
+            </header>
         
-        <div>
-          <p>
-            Welcome, {user.displayName} ({user.email})
-          </p>
-          <Link href="http://localhost:3000/week-9/protected"></Link>
+        {user ? 
+        ( 
+          <div>
+            <div>
+              <p>
+                Welcome, {user.displayName} ({user.email})
+              </p>
+              <img src={user.photoUrl} className="ml-2 py-4 w-10 h-10"></img>
+              <Link href="http://localhost:3000/week-9/shopping-list" className='underline text-blue'>Shopping List Page</Link>
+            </div>
 
+            <div>
+              <button 
+                type="button"
+                className="text-lg bg-blue-600 text-white rounded px-2 py-1 ml-5"
+                onClick={handleSignOut}>
+                Sign Out
+              </button>
+            </div>
+          </div>
+        ) 
+        : 
+        (
+          <div>
+            <button 
+                type="button"
+                className="text-lg bg-blue-600 text-white rounded px-2 py-1 ml-5"
+                onClick={handleSignIn}>
+                Sign In
+            </button>
+          </div>
+        ) }
 
-          
-        </div>
       </main>
     )
 }
--- app/week-9/shopping-list/page.js
+++ app/week-9/shopping-list/page.js
@@ -1,10 +1,31 @@
+"use client"
+import { useUserAuth } from "../_utils/auth-context"
+import Link from "next/link"
+
 export default function ProtectedPage() {
 
+        //custom hook we created
+        const {user} = useUserAuth();
+
     return (
-        <main>
+        <main className="mb-5">
             <header>
                 <h1>Protected Page</h1>
             </header>
+
+            {user ? 
+            (
+                <div>
+                    <p>Hello, {user.displayName}, your user ID is: {user.uid}</p>                 
+                </div>
+            ) 
+            : 
+            (
+                <div>
+                    <p>You must be logged in to view this page</p>
+                    <Link href="http://localhost:3000/week-9/shopping-list">Click here to return to the Sign In page.</Link>
+                </div>
+            )}
         </main>
     )
 }
